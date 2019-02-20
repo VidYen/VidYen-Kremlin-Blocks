38652e22abc4ce87f5f1c64abbf0b7e9bbea5db3
Felty <40067341+VidYen@users.noreply.github.com>
2019-02-20 15:54:22 -0500

    update

--- vidyen-russian-blocks/sbf_admin_more.php
+++
@@ -1,9 +0,0 @@
-
-<hr>
-
-<?php _e( 'Soon we are going to add code for Donation Form', 'simple-bitcoin-faucets' ); ?>,
- <?php _e( 'more games, including betting ones, like', 'simple-bitcoin-faucets' ); ?> 
- <a target=_blank href="https://codepen.io/Clowerweb/pen/FDcxe">BlackJack</a> ,
- <?php _e( 'and', 'simple-bitcoin-faucets' ); ?> <?php _e( 'much more', 'simple-bitcoin-faucets' ); ?>...
-<br>
-<br>
