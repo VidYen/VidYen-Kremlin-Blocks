9b064bc987b8b6f77dddea93ef91fd2a3bd801ac
Felty <40067341+VidYen@users.noreply.github.com>
2019-02-27 15:33:29 -0500

    db fix

--- vidyen-kremlin-blocks/vidyen-kremlin-blocks.php
+++ vidyen-kremlin-blocks/vidyen-kremlin-blocks.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://vidyen.com
  * Description: A Tetris like game for the VidYen Point System
  * Author: VidYen, LLC
- * Version: 0.0.5
+ * Version: 0.0.7
  * Author URI: http://vidyen.com
  * License: GPLv2
 */
@@ -17,10 +17,10 @@ include_once( dirname(__FILE__) . DIRECTORY_SEPARATOR .  'vidyen-kremlin-blocks-
 //Shortcode include
 include_once( dirname(__FILE__) . DIRECTORY_SEPARATOR .  'includes/shortcodes/vidyen-kremlin-blocks-shortcode.php'); //SBFG_WP_get_poker_init
 
-register_activation_hook(__FILE__, 'videyen_kremlin_blocks_sql_install');
+register_activation_hook(__FILE__, 'vidyen_kremlin_blocks_sql_install');
 
 //Install the SQL tables for VYPS.
-function videyen_kremlin_blocks_sql_install()
+function vidyen_kremlin_blocks_sql_install()
 {
     global $wpdb;
 
@@ -29,8 +29,8 @@ function videyen_kremlin_blocks_sql_install()
 
 		//NOTE: I have the mind to make mediumint to int, but I wonder if you get 8 million log transactios that you should consider another solution than VYPS.
 
-		//videyen_kremlin_blocks table creation
-    $table_name_kremlin = $wpdb->prefix . 'videyen_kremlin_blocks';
+		//vidyen_kremlin_blocks table creation
+    $table_name_kremlin = $wpdb->prefix . 'vidyen_kremlin_blocks';
 
     $sql = "CREATE TABLE {$table_name_kremlin} (
   		id mediumint(9) NOT NULL AUTO_INCREMENT,
@@ -48,7 +48,7 @@ function videyen_kremlin_blocks_sql_install()
 		//Default data
 		$data_insert = [
 				'input_point_id' => 1,
-        'input_amount' => 1,
+        'input_point_amount' => 1,
 				'output_point_id' => 2,
 				'win_multi' => 1,
 		];
