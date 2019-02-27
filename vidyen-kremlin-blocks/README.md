a87a9a972f3cd6d79a37835574ccdaa4dce1bb83
Felty <40067341+VidYen@users.noreply.github.com>
2019-02-27 12:09:24 -0500

    update

--- vidyen-kremlin-blocks/README.md
+++ vidyen-kremlin-blocks/README.md
@@ -1,20 +1,15 @@
 === VidYen Kremlin Blocks ===
 Contributors: vidyen, felty, trof
 Tags: bitcoin,faucet,satoshi,games,poker,visitor rewarder, referrals, tetris, blockrain,2048,lines,minesweeper, video poker, bubble shooter, videopoker,satoshi bonds, shortcode,rain captcha,surfer rewarder,cryptocurrency,crypto-currency,crypto currency,satoshi,BTC,Dogecoin,DOGE,Litecoin,LTC,Ethereum,ETH,Primecoin,XPM,DASH,Peercoin,PPC
-Requires at least: 2.0
-Tested up to: 5.0.3
+Requires at least: 5.0.0
+Tested up to: 5.1.0
 Stable tag: 1.1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-
-
 == Description ==
 
 Fork of the GPLv2 Simple Bitcoin Faucet for WordPress with just the "Tetris" clone game extracted for use with the VidYen Point System
-
-
-
 
 == Changelog ==
 
--- vidyen-kremlin-blocks/vidyen-kremlin-blocks.php
+++ vidyen-kremlin-blocks/vidyen-kremlin-blocks.php
@@ -11,3 +11,41 @@
 
 //This might all we need for riht now.
 include_once( dirname(__FILE__) . DIRECTORY_SEPARATOR .  'includes/shortcodes/vidyen-kremlin-blocks-shortcode.php'); //SBFG_WP_get_poker_init
+
+register_activation_hook(__FILE__, 'videyen_kremlin_blocks_sql_install');
+
+//Install the SQL tables for VYPS.
+function videyen_kremlin_blocks_sql_install()
+{
+    global $wpdb;
+
+		//I have no clue why this is needed. I should learn, but I wasn't the original author. -Felty
+		$charset_collate = $wpdb->get_charset_collate();
+
+		//NOTE: I have the mind to make mediumint to int, but I wonder if you get 8 million log transactios that you should consider another solution than VYPS.
+
+		//videyen_kremlin_blocks table creation
+    $table_name_poker = $wpdb->prefix . 'videyen_kremlin_blocks';
+
+    $sql = "CREATE TABLE {$table_name_poker} (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		input_point_id mediumint(9) NOT NULL,
+    input_amount mediumint(9) NOT NULL,
+    output_point_id mediumint(9) NOT NULL,		
+		win_multi float(53) NOT NULL,
+		PRIMARY KEY  (id)
+        ) {$charset_collate};";
+
+    require_once (ABSPATH . 'wp-admin/includes/upgrade.php'); //I never did investigate why the original outsource dev used this.
+
+    dbDelta($sql);
+
+		//Default data
+		$data_insert = [
+				'point_id' => 1,
+				'maximum_bet' => 100,
+				'win_multi' => 1,
+		];
+
+		$wpdb->insert($table_name_poker, $data_insert);
+}
